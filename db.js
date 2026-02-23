b33ac77d797003f28436adbb677a9e063dd688e6
ATEF GHAYAD <abohusam.ja.555@gmail.com>
2026-02-23 14:22:03 +0300

    Update DB config for Cloud

--- db.js
+++ db.js
@@ -5,17 +5,15 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-});
+const isProduction = process.env.NODE_ENV === "production";
 
-pool
-  .connect()
-  .then(() => console.log("Successfully connnected to PostgreSQL Database!"))
-  .catch((err) => console.error("Database Connection Failed!", err));
+const connectionString =
+  process.env.DATABASE_URL ||
+  `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 
 export default pool;
